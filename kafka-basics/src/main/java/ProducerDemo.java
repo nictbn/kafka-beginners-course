12a76f2e697468a6ce097961c784bf083a0d9117
Nicu Taban <nictbndeveloper@gmail.com>
2023-08-06 15:11:37 +0300

    Sends message to the demo_java topic

--- kafka-basics/src/main/java/ProducerDemo.java
+++ kafka-basics/src/main/java/ProducerDemo.java
@@ -1,5 +1,40 @@
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Properties;
+
 public class ProducerDemo {
+
+    private static final Logger log = LoggerFactory.getLogger(ProducerDemo.class);
+    public static final String BOOTSTRAP_SERVERS = "bootstrap.servers";
+    public static final String LOCALHOST_BOOSTRAP_SERVER = "127.0.0.1:9092";
+    public static final String KEY_SERIALIZER = "key.serializer";
+    public static final String VALUE_SERIALIZER = "value.serializer";
+    public static final String TOPIC = "demo_java";
+
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        log.info("Hello World!");
+
+        // Create Producer Properties
+        Properties properties = new Properties();
+        properties.setProperty(BOOTSTRAP_SERVERS, LOCALHOST_BOOSTRAP_SERVER);
+        properties.setProperty(KEY_SERIALIZER, StringSerializer.class.getName());
+        properties.setProperty(VALUE_SERIALIZER, StringSerializer.class.getName());
+
+        // Create the Producer
+        KafkaProducer<String, String> producer = new KafkaProducer<>(properties);
+
+        // Create a Producer Record
+        ProducerRecord<String, String> producerRecord = new ProducerRecord<>(TOPIC, "hello world");
+
+        // Send the Data
+        producer.send(producerRecord);
+
+        // Flush and close the producer
+        producer.flush();
+        producer.close();
     }
 }
